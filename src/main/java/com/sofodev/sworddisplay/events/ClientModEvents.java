f9e5735a15c0f964ea1dfbdd372e4ea56d9e1558
Sokratis Fotkatzikis <sokratis12GR@users.noreply.github.com>
2025-10-10 00:33:04 +0300

    Removed redundant code

--- src/main/java/com/sofodev/sworddisplay/events/ClientModEvents.java
+++ src/main/java/com/sofodev/sworddisplay/events/ClientModEvents.java
@@ -1,8 +1,6 @@
 package com.sofodev.sworddisplay.events;
 
 import com.sofodev.sworddisplay.blocks.TESRSwordDisplay;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -10,7 +8,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import static com.sofodev.sworddisplay.SwordDisplay.MODID;
 import static com.sofodev.sworddisplay.registry.ModBlocks.SWORD_DISPLAY_TYPE;
-import static com.sofodev.sworddisplay.registry.ModBlocks.registryHelper;
 import static net.minecraftforge.api.distmarker.Dist.CLIENT;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = CLIENT)
@@ -19,10 +16,6 @@ public class ClientModEvents {
     @SubscribeEvent
     public static void doClientStuff(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
-//            registryHelper.forEach(ro ->
-//                    ItemBlockRenderTypes.setRenderLayer(ro.getCaseBlock().get(), RenderType.cutout())
-//            );
-
             BlockEntityRenderers.register(SWORD_DISPLAY_TYPE.get(), TESRSwordDisplay::new);
         });
     }
